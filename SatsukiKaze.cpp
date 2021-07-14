9b25e48cc3249060242708b68420e59aeb4aff13
MorohoshiMari <yamaandtanbo@gmail.com>
2021-07-14 23:51:05 +0900

    SatsukiKaze,Hanaクラスを宣言

---
+++ SatsukiKaze.cpp
@@ -0,0 +1,24 @@
+#include "SatsukiKaze.h"
+
+SatsukiKaze::SatsukiKaze( void )
+{
+    setSName( "冴月麟(花)" );
+    setHighFSpeed( 3.0 );
+    setLowFSpeed( 1.5 );
+    setMainShotPower( 25 );
+    setSubShotPower( 30 );
+    setSubShotFSpeed( 8 );
+    setSubShotInterval( 5 );
+    setNFeverFrames_Bomb( 315 );
+    setGodMode( false );
+}
+
+SatsukiKaze::~SatsukiKaze()
+{
+
+}
+
+void SatsukiKaze::spellCard( void )
+{
+
+}
