b19ac5cbb3afef2f0aa815896f5f1604a55184f2
MorohoshiMari <yamaandtanbo@gmail.com>
2021-07-16 00:03:22 +0900

    developブランチにChara関係をpush

--- Chara/SatsukiHana.cpp
+++ Chara/SatsukiHana.cpp
@@ -1,24 +1,24 @@
-#include "SatsukiKaze.h"
+#include "SatsukiHana.hpp"
 
-SatsukiKaze::SatsukiKaze( void )
+SatsukiHana::SatsukiHana( void )
 {
     setSName( "冴月麟(花)" );
     setHighFSpeed( 3.0 );
     setLowFSpeed( 1.5 );
     setMainShotPower( 25 );
-    setSubShotPower( 30 );
+    setSubShotPower( 35 );
     setSubShotFSpeed( 8 );
     setSubShotInterval( 5 );
-    setNFeverFrames_Bomb( 315 );
+    setNFeverFrames_Bomb( 312 );
     setGodMode( false );
 }
 
-SatsukiKaze::~SatsukiKaze()
+SatsukiHana::~SatsukiHana()
 {
 
 }
 
-void SatsukiKaze::spellCard( void )
+void SatsukiHana::spellCard( void )
 {
 
 }
