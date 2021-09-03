eb903cc6a8af810902d3f43dc67508bd75637b4c
ojiji <okachan2003@outlook.jp>
2021-09-03 18:28:42 +0900

    Add title logo

--- src/ImageManager.hpp
+++ src/ImageManager.hpp
@@ -13,7 +13,7 @@ public:
     ImageManager(SDL_Renderer* renderer_handle, std::string base_path);
     virtual ~ImageManager();
     void Render(SDL_Renderer* renderer_handle, ImageID id, const SDL_Rect* srcrect, const SDL_Rect* dstrect);
-    void Render(SDL_Renderer* renderer_handle, ImageID image_id, const SDL_Rect* srcrect, const SDL_Rect* dstrect, uint8_t alpha);
+    void Render(SDL_Renderer* renderer_handle, ImageID image_id, const SDL_Rect* srcrect, const SDL_Rect* dstrect, uint8_t alpha, double angle);
 };
 
 #endif /* _IMAGE_MANAGER_H_ */
