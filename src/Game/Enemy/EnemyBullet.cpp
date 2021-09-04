d21a3ad79c6e29fd4cd0c1b541bb5e509d0fc565
takotsubo <tsumikiryo417@gmail.com>
2021-09-04 21:26:06 +0900

    ベクトル指定でのupdatePlaceを作成

--- src/Game/Enemy/EnemyBullet.cpp
+++ src/Game/Enemy/EnemyBullet.cpp
@@ -1,23 +1,25 @@
+#include <cmath>
 #include"EnemyBullet.hpp"
+const double PI = 3.141593;
 
-EnemyBullet::EnemyBullet( void )
+EnemyBullet::EnemyBullet(void)
 {
 
 }
 
-EnemyBullet::EnemyBullet( float x, float y, float angle )
+EnemyBullet::EnemyBullet(float x, float y, float angle)
 {
-    setX( x );
-    setY( y );
-    setAngle( angle );
+    setX(x);
+    setY(y);
+    setAngle(angle);
 }
 
-EnemyBullet::EnemyBullet( int enemyBulletID, float x, float y, float angle )
+EnemyBullet::EnemyBullet(int enemyBulletID, float x, float y, float angle)
 {
-    setEnemyBulletID( enemyBulletID );
-    setX( x );
-    setY( y );
-    setAngle( angle );
+    setEnemyBulletID(enemyBulletID);
+    setX(x);
+    setY(y);
+    setAngle(angle);
 }
 
 EnemyBullet::~EnemyBullet()
@@ -25,9 +27,16 @@ EnemyBullet::~EnemyBullet()
 
 }
 
-void EnemyBullet::updatePlace( float dx, float dy, float dAngle )
+void EnemyBullet::updatePlace(float dx, float dy, float dAngle)
 {
-    setX( getX() + dx );
-    setY( getY() + dy );
-    setAngle( getAngle() + dAngle );
+    setX(getX() + dx);
+    setY(getY() + dy);
+    setAngle(getAngle() + dAngle);
+}
+
+void EnemyBullet::updatePlaceWithVector(float displacement, float dAngle)
+{
+    setAngle(getAngle() + dAngle);
+    setX(getX() - displacement * std::sin((360 - getAngle()) * PI / 180.0));
+    setY(getY() - displacement * std::cos((360 - getAngle()) * PI / 180.0));
 }
