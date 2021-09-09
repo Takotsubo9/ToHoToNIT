67bafd60afc0b896427c1c36129c5f7d95334b71
MorohoshiMari <yamaandtanbo@gmail.com>
2021-09-09 16:49:20 +0900

    HP処理の記述・Enemyクラスにx,y座標搭載

--- src/Game/Enemy/Enemy.cpp
+++ src/Game/Enemy/Enemy.cpp
@@ -10,7 +10,31 @@ Enemy::Enemy( int enemyID )
     setEnemyID( enemyID );
 }
 
+Enemy::Enemy( int enemyID, float x, float y )    //生成時にいきなりスクリーンに映らないように、画面外指定
+{
+    setEnemyID( enemyID );
+    setX( -1 );
+    setY( -1 );
+}
+
 Enemy::~Enemy()
 {
 
+}
+
+void Enemy::damageHP( int damagedHP )
+{
+    setHP( getHP() + ( damagedHP * getDefense() ) );
+}
+
+void Enemy::restHP( int restedHP )
+{
+    if( ( getHP() + restedHP ) > getMaxHP() )
+    {
+        setHP( getMaxHP() );
+    }
+    else
+    {
+        setHP( getHP() + restedHP );
+    }
 }
--- src/Game/Enemy/Enemy.hpp
+++ src/Game/Enemy/Enemy.hpp
@@ -1,29 +1,41 @@
 #ifndef _ENEMY_H_
 #define _ENEMY_H_
 
+#include "EnemyBarrage.hpp"
+
 class Enemy
 {
 private:
     int enemyID;
+    int maxHP;
     int HP;
+    float x;
+    float y;
     int enemyHitBoxRadius;    //当たり判定半径
     float defense;    //敵の防御率(0-1の間)
 protected:
-    void setHP( int HP ){ this->HP = HP; }
+    void setMaxHP( int maxHP ){ this->maxHP = maxHP; }
     void setEnemyHitBoxRadius( int enemyHitBoxRadius ){ this->enemyHitBoxRadius = enemyHitBoxRadius; }
 public:
     Enemy( void );
     Enemy( int enemyID );
+    Enemy( int enemyID, float x, float y );    //使うんだったら使って
     virtual ~Enemy();
     void setEnemyID( int enemyID ){ this->enemyID = enemyID; }
     int getEnemyID( void ){ return enemyID; }
+    int getMaxHP( void ){ return maxHP; }
+    void setHP( int HP ){ this->HP = HP; }
     int getHP( void ){ return HP; }
+    void setX( float x ){ this->x = x; }
+    float getX( void ){ return x; }
+    void setY( float y ){ this->y = y; }
+    float getY( void ){ return y; }
     int getEnemyHitBoxRadius( void ){ return enemyHitBoxRadius; }
     void setDefense( float defense ){ this->defense = defense; }
     float getDefense( void ){ return defense; }
 
-    virtual void movement( void ) = 0;
-    virtual void shot( void ) = 0;
+    void damageHP( int damagedHP );
+    void restHP( int restedHP );
 };
 
 #endif /* _ENEMY_H_ */
