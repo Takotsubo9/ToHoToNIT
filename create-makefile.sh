67977d81d8cafe48c6b973e574981b4caa123bfb
ojiji <okachan2003@outlook.com>
2022-02-01 01:14:53 +0900

    Improve makefile generation shell script

--- create-makefile.sh
+++ create-makefile.sh
@@ -3,11 +3,15 @@ for file in `\find -name '*.cpp'`; do
     filelist="${filelist} ${file}"
 done
 
+echo "CXX = g++"
 echo "CXXFLAGS = -Wall -Wextra -static-libgcc -static-libstdc++" > makefile
 echo "LDFLAGS = -lm -lSDL2 -lSDL2_image" >> makefile
-echo "OBJECTS = "${filelist} >> makefile
+echo "SRCS = "${filelist} >> makefile
+echo "OBJECTS = ""$""(SRCS:%cpp=%o)" >> makefile
 echo "all : ""$""(OBJECTS) bin" >> makefile
-echo "	g++ ""$""(CXXFLAGS) -o bin/Touhou-Koumatou ""$""(OBJECTS) ""$""(LDFLAGS)" >> makefile
+echo "	""$""(CXX) ""$""(CXXFLAGS) -o bin/Touhou-Koumatou ""$""(OBJECTS) ""$""(LDFLAGS)" >> makefile
+echo "%.o: %.c" >> makefile
+echo "	""$""(CXX) ""$""(CXXFLAGS) -c ""$""< -o ""$""@ ""$""(LDFLAGS)" >> makefile
 echo "" >> makefile
 echo "bin :" >> makefile
 echo "	mkdir bin" >> makefile
@@ -16,3 +20,4 @@ echo ".PHONY : clean" >> makefile
 echo "remake : clean all" >> makefile
 echo "clean :" >> makefile
 echo "	-rm -rf bin/Touhou-Koumatou" >> makefile
+echo "	-rm ""$""(OBJECTS)" >> makefile
