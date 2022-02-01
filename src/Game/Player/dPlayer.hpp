8eb172b7621c661c23c47fedc81cfd742f4da217
ojiji <okachan2003@outlook.com>
2022-02-01 17:51:09 +0900

    dPlayerの初期位置を決定

--- src/Game/Player/dPlayer.hpp
+++ src/Game/Player/dPlayer.hpp
@@ -16,10 +16,7 @@ private:
     float y;
     const int hitBoxRadius = 5;    //キャラの当たり判定半径(pxl) 全キャラで不変
 public:
-    dPlayer() {
-        this->x = 100;
-        this->y = 100;
-    }
+    dPlayer();
     virtual ~dPlayer() {
 
     }
@@ -41,19 +38,8 @@ public:
     float getY( void ){ return y; }
     int getHitBoxRadius( void ){ return hitBoxRadius; }
 
-    void move(float x, float y, bool isSlow) {
-        float speed = isSlow ? this->player->getCharacter()->getLowFSpeed() : this->player->getCharacter()->getHighFSpeed();
-        this->x += x * 1.5 * speed;
-        this->y += y * 1.5 * speed;
-        this->x = std::max(std::min(static_cast<double>(this->x), (32 + 385) * 1.5), 32 * 1.5);
-        this->y = std::max(std::min(static_cast<double>(this->y), (16 + 450) * 1.5), 16 * 1.5);
-    }
-    void Draw(GameWindow * game_window) {
-        SDL_Rect player_rect = {static_cast<int>(this->x), static_cast<int>(this->y), 30, 40};
-        player_rect.x -= (player_rect.w / 2);
-        player_rect.y -= (player_rect.h / 2);
-        game_window->FillRect(0xff, 0xff, 0xff, 0xff, &player_rect);
-    }
+    void move(float x, float y, bool isSlow);
+    void Draw(GameWindow * game_window);
 };
 
 #endif /* _DPLAYER_H_ */
