1d06e7844cb79997fda389c72ab5dc561a422238
ojiji <okachan2003@outlook.com>
2022-03-26 23:25:40 +0900

    当たり判定の調整

--- src/Game/Stage/Stage.cpp
+++ src/Game/Stage/Stage.cpp
@@ -7,7 +7,7 @@ Stage::Stage(Player * player) {
 void Stage::Judge(GameWindow* game_window) {
     //もろもろの当たり判定を判定する関数
     for(auto item = this->item_list.begin(); item != this->item_list.end();) {
-        if(std::abs(dplayer.getX() - item->getX()) <= 20 && std::abs(dplayer.getY() - item->getY()) <= 20) {
+        if(std::abs(dplayer.getX() - item->getX()) <= 20 && std::abs(dplayer.getY() - item->getY()) <= 30) {
             item->Effect(&dplayer);
             item = this->item_list.erase(item);
             game_window->PlaySE(SoundEffectID::get_item);
