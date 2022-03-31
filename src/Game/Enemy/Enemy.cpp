f74eaf7501dab34efc1bf66116ff02365f53742c
ojiji <okachan2003@outlook.com>
2022-04-01 01:08:51 +0900

    EnemyクラスのコンストラクタのWarning修正

--- src/Game/Enemy/Enemy.cpp
+++ src/Game/Enemy/Enemy.cpp
@@ -9,13 +9,15 @@ Enemy::Enemy( void )
 Enemy::Enemy( int enemyID )
 {
     setEnemyID( enemyID );
+    setX( -1 );
+    setY( -1 );
 }
 
 Enemy::Enemy( int enemyID, float x, float y )    //生成時にいきなりスクリーンに映らないように、画面外指定
 {
     setEnemyID( enemyID );
-    setX( -1 );
-    setY( -1 );
+    setX( x );
+    setY( y );
 }
 
 Enemy::~Enemy()
